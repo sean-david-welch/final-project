068b964368977e5110eb3825caec34f7999daea7
Sean Welch <seanwelch51199@icloud.com>
2024-11-10 13:10:04 +0000

    main and index

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -10,13 +10,9 @@ fun BaseTemplate(bodyFn: BODY.() -> Unit): String {
             meta { charset = "UTF-8" }
             meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
             script { type = "text/javascript"; src = "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio" }
-            script { type = "text/javascript"; unsafe { +""" tailwind.config = { darkMode: 'class', theme: { extend: { colors: { primary: '#4F46E5', } } } } """ } }
-
             script { src = "https://unpkg.com/alpinejs@3.13.3/dist/cdn.min.js"; defer = true }
             script { src = "https://unpkg.com/htmx.org@1.9.10"; defer = true }
-
             link { href = "/index.css"; rel = "stylesheet" }
-
             style { unsafe { +""" @layer base { html { font-family: system-ui, sans-serif; } } """ } }
         }
         body {
