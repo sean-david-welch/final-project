36e4414dde837e96d45c25f3c2af00c746d2295f
Sean Welch <seanwelch51199@icloud.com>
2024-11-10 13:56:05 +0000

    output

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -11,7 +11,7 @@ fun BaseTemplate(bodyFn: BODY.() -> Unit): String {
             meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
             script { src = "https://unpkg.com/alpinejs@3.13.3/dist/cdn.min.js"; defer = true }
             script { src = "https://unpkg.com/htmx.org@1.9.10"; defer = true }
-            link { href = "/static/styles/index.css"; rel = "stylesheet" }
+            link { href = "/static/styles/output.css"; rel = "stylesheet" }
             style { unsafe { +""" @layer base { html { font-family: system-ui, sans-serif; } } """ } }
         }
         body {
