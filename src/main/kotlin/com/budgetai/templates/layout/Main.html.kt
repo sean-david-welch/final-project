cff41d1879a84dfd105885591c9c9be115ec11f5
Sean Welch <seanwelch51199@icloud.com>
2024-11-10 13:51:08 +0000

    tailwind build

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -9,10 +9,9 @@ fun BaseTemplate(bodyFn: BODY.() -> Unit): String {
         head {
             meta { charset = "UTF-8" }
             meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
-            script { type = "text/javascript"; src = "https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio" }
             script { src = "https://unpkg.com/alpinejs@3.13.3/dist/cdn.min.js"; defer = true }
             script { src = "https://unpkg.com/htmx.org@1.9.10"; defer = true }
-            link { href = "/index.css"; rel = "stylesheet" }
+            link { href = "/static/styles/index.css"; rel = "stylesheet" }
             style { unsafe { +""" @layer base { html { font-family: system-ui, sans-serif; } } """ } }
         }
         body {
