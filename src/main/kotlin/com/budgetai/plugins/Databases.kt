ccbf645cde8fb3cadf1e8d4a2988e7dc894d9832
Sean Welch <seanwelch51199@icloud.com>
2024-11-14 19:39:03 +0000

    return db

--- src/main/kotlin/com/budgetai/plugins/Databases.kt
+++ src/main/kotlin/com/budgetai/plugins/Databases.kt
@@ -3,11 +3,11 @@ package com.budgetai.plugins
 import org.jetbrains.exposed.sql.Database
 import java.io.File
 
-fun configureDatabases() {
+fun configureDatabases(): Database {
     val projectDir = File("src/main/kotlin/com/budgetai")
     val dbFile = projectDir.resolve("database.db")
 
-    Database.connect(
+    return Database.connect(
         url = "jdbc:sqlite:${dbFile.absolutePath}?journal_mode=WAL&foreign_keys=ON",
         driver = "org.sqlite.JDBC"
     )
