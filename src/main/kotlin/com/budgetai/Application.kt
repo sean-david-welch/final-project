20be5557057de368858232263f8896f8482d47b5
Sean Welch <seanwelch51199@icloud.com>
2024-11-17 18:21:05 +0000

    add seed code to application

--- src/main/kotlin/com/budgetai/Application.kt
+++ src/main/kotlin/com/budgetai/Application.kt
@@ -1,9 +1,11 @@
 package com.budgetai
 
+import com.budgetai.lib.DataSeeder
 import com.budgetai.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module).start(wait = true)
@@ -11,6 +13,16 @@ fun main() {
 
 fun Application.module() {
     DatabaseConfig.initialize()
+
+    val shouldSeedData = environment.config.propertyOrNull("ktor.database.seed")?.getString()?.toBoolean() ?: false
+    if (shouldSeedData) {
+        log.info("Seeding database...")
+        transaction {
+            DataSeeder().seed(this)
+        }
+        log.info("Database seeding completed")
+    }
+
     configureSecurity()
     configureHTTP()
     configureSerialization()
