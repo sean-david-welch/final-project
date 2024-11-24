cf9578e7df3ca2ce79d0e1b7b9f4dcc0a596a6b8
Sean Welch <seanwelch51199@icloud.com>
2024-11-24 21:32:50 +0000

    line securitt

--- src/main/kotlin/com/budgetai/plugins/Security.kt
+++ src/main/kotlin/com/budgetai/plugins/Security.kt
@@ -17,11 +17,7 @@ fun Application.configureSecurity() {
         jwt {
             realm = jwtRealm
             verifier(
-                JWT
-                    .require(Algorithm.HMAC256(jwtSecret))
-                    .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain)
-                    .build()
+                JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtDomain).build()
             )
             validate { credential ->
                 if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
