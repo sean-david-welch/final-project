60f972fc98266e8d9f87ec8918a5cf459965f6f6
Sean Welch <seanwelch51199@icloud.com>
2024-11-24 20:41:21 +0000

    enums

---
+++ src/main/kotlin/com/budgetai/models/Enums.kt
@@ -0,0 +1,13 @@
+package com.budgetai.models
+
+enum class CategoryType {
+    EXPENSE, INCOME
+}
+
+enum class InsightType {
+    BUDGET_ANALYSIS, ITEM_ANALYSIS, SAVING_SUGGESTION, GENERAL_ADVICE
+}
+
+enum class Sentiment {
+    POSITIVE, NEGATIVE, NEUTRAL
+}
