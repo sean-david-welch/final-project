101f56f1e11001290edade1431354a7d7a001177
Sean Welch <seanwelch51199@icloud.com>
2024-11-24 20:56:13 +0000

    configure serialization

--- src/main/kotlin/com/budgetai/plugins/Serialization.kt
+++ src/main/kotlin/com/budgetai/plugins/Serialization.kt
@@ -1,14 +1,17 @@
 package com.budgetai.plugins
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.serialization.jackson.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        jackson { enable(SerializationFeature.INDENT_OUTPUT) }
-        json()
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+        })
     }
 }
