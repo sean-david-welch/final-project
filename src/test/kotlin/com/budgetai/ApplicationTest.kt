3de57dc04c8073b42d9dbe9d0be574d3b7c04ed6
Sean Welch <seanwelch51199@icloud.com>
2024-11-24 12:37:18 +0000

    Add configuration to root application test

--- src/test/kotlin/com/budgetai/ApplicationTest.kt
+++ src/test/kotlin/com/budgetai/ApplicationTest.kt
@@ -1,6 +1,9 @@
 package com.budgetai
 
+import com.budgetai.plugins.configureHTTP
 import com.budgetai.plugins.configureRouting
+import com.budgetai.plugins.configureSecurity
+import com.budgetai.plugins.configureSerialization
 import io.ktor.server.testing.*
 import kotlin.test.Test
 
@@ -8,6 +11,9 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            configureSecurity()
+            configureHTTP()
+            configureSerialization()
             configureRouting()
         }
     }
