0d6519f4639377db6f61a2862640706c40a667b4
Sean Welch <seanwelch51199@icloud.com>
2024-11-24 15:13:08 +0000

    dto

--- src/main/kotlin/com/budgetai/models/DataTransfer.kt
+++
@@ -1,9 +0,0 @@
-package com.budgetai.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UserDTO(
-    val id: Int = 0, val email: String, val name: String, val createdAt: String? = null
-)
-
--- src/main/kotlin/com/budgetai/models/Users.kt
+++ src/main/kotlin/com/budgetai/models/Users.kt
@@ -1,13 +1,21 @@
 package com.budgetai.models
 
+import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.kotlin.datetime.CurrentTimestamp
 import org.jetbrains.exposed.sql.kotlin.datetime.timestamp
 
+// models
 object Users : IntIdTable("users") {
     val email = varchar("email", 255).uniqueIndex()
     val passwordHash = varchar("password_hash", 255)
     val name = varchar("name", 100)
     val createdAt = timestamp("created_at").defaultExpression(CurrentTimestamp)
 }
+
+// DTO
+@Serializable
+data class UserDTO(
+    val id: Int = 0, val email: String, val name: String, val createdAt: String? = null
+)
 
