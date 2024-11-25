280c2b590a66a7da234a7461874a1e1b7f06e663
Sean Welch <seanwelch51199@icloud.com>
2024-11-25 19:17:45 +0000

    lint footer

--- src/main/kotlin/com/budgetai/templates/components/Footer.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Footer.html.kt
@@ -2,27 +2,17 @@ package com.budgetai.templates.components
 
 import kotlinx.html.*
 
-data class SimpleFooterLink(
-    val text: String, val href: String
-)
+data class SimpleFooterLink(val text: String, val href: String)
 
 fun FlowContent.Footer(links: List<SimpleFooterLink> = listOf()) {
-    footer {
-        classes = setOf("footer")
-        div {
-            classes = setOf("footer-container")
-            div {
-                classes = setOf("footer-copyright") +"© ${java.time.Year.now().value} BudgetAI"
-            }
-            div {
-                classes = setOf("footer-links-container")
-                links.forEach { link ->
-                    a(href = link.href) {
-                        classes = setOf("footer-link")
-                        +link.text
-                    }
-                }
-            }
-        }
-    }
+   footer(classes = "footer") {
+       div(classes = "footer-container") {
+           div(classes = "footer-copyright") { +"© ${java.time.Year.now().value} BudgetAI" }
+           div(classes = "footer-links-container") {
+               links.forEach { link ->
+                   a(href = link.href, classes = "footer-link") { +link.text }
+               }
+           }
+       }
+   }
 }
