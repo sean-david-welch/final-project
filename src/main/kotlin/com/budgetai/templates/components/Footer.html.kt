9c87d29f27ba068fd6f67202fadd0742fa49b644
Sean Welch <seanwelch51199@icloud.com>
2024-11-25 18:47:39 +0000

    refactor footer

--- src/main/kotlin/com/budgetai/templates/components/Footer.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Footer.html.kt
@@ -7,55 +7,22 @@ data class SimpleFooterLink(
 )
 
 fun FlowContent.Footer(
-    companyName: String = "Your Company", links: List<SimpleFooterLink> = listOf(), showThemeToggle: Boolean = false
+    links: List<SimpleFooterLink> = listOf()
 ) {
     footer {
         classes = setOf("footer")
-
         div {
             classes = setOf("footer-container")
-
             div {
                 classes = setOf("footer-copyright")
-                +"© ${java.time.Year.now().value} $companyName"
+                +"© ${java.time.Year.now().value} BudgetAI"
             }
-
             div {
                 classes = setOf("footer-links-container")
-
                 links.forEach { link ->
                     a(href = link.href) {
                         classes = setOf("footer-link")
                         +link.text
-                    }
-                }
-
-                if (showThemeToggle) {
-                    button {
-                        attributes["data-x-on:click"] = "darkMode = !darkMode"
-                        classes = setOf("theme-toggle-button")
-
-                        span {
-                            attributes["data-x-show"] = "darkMode"
-                            unsafe {
-                                +"""
-                                <svg class="theme-toggle-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-                                </svg>
-                                """
-                            }
-                        }
-
-                        span {
-                            attributes["data-x-show"] = "!darkMode"
-                            unsafe {
-                                +"""
-                                <svg class="theme-toggle-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-                                </svg>
-                                """
-                            }
-                        }
                     }
                 }
             }
