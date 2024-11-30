4e84375457d9a8031d2dea18fc3e5b62504c7ffb
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 12:28:21 +0000

    create dashboard routes

---
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -0,0 +1,17 @@
+package com.budgetai.routes.templates
+
+import com.budgetai.templates.pages.createDashboardPage
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.dashboardRoutes() {
+    route("/dashboard") {
+
+    get("/") {
+        call.respondText(
+            text = createDashboardPage(), contentType = ContentType.Text.Html
+        )
+    }
+    }
+}
