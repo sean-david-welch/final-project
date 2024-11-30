335491c1ec933f861d0607c726588ecbc6e5315d
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 15:36:35 +0000

    favicon

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -10,11 +10,10 @@ fun BaseTemplate(bodyFn: BODY.() -> Unit) = "<!DOCTYPE html>" + createHTML().htm
         meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
         script { src = "/webjars/alpinejs/3.14.3/dist/cdn.js"; defer = true }
         script { src = "/webjars/htmx.org/2.0.3/dist/htmx.min.js"; defer = true }
+        link { href = "/static/images/favicon.ico"; rel = "icon"; type = "image/x-icon" }
         link { href = "/static/styles/output.css"; rel = "stylesheet" }
-        link {
-            href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-            rel = "stylesheet"
-        }
+        link { href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"; rel = "stylesheet" }
+
     }
     body(classes = "font-inter layout-base coontainer-base") {
         Navbar()
