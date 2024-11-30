2256f2ce3d6cb2683af4272db88e19fa1944ef5a
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 10:03:58 +0000

    home page

--- src/main/kotlin/com/budgetai/templates/pages/Home.html.kt
+++ src/main/kotlin/com/budgetai/templates/pages/Home.html.kt
@@ -2,7 +2,12 @@ package com.budgetai.templates.pages
 
 import com.budgetai.templates.layout.BaseTemplate
 import com.budgetai.templates.layout.Navbar
+import kotlinx.html.classes
+import kotlinx.html.h1
+import kotlinx.html.main
 
 fun HomeTemplate() = BaseTemplate {
-    Navbar()
+    main(classes = "dashboard-layout") {
+       h1() { +"hello" }
+    }
 }
