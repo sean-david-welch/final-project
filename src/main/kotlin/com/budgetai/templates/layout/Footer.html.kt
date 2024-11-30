179970a251645d48010ed066baa3c878616874c6
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 09:49:28 +0000

    refactor footer

--- src/main/kotlin/com/budgetai/templates/layout/Footer.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Footer.html.kt
@@ -2,17 +2,21 @@ package com.budgetai.templates.layout
 
 import kotlinx.html.*
 
-data class SimpleFooterLink(val text: String, val href: String)
+data class FooterLink(val text: String, val href: String)
 
-fun FlowContent.Footer(links: List<SimpleFooterLink> = listOf()) {
-   footer(classes = "footer") {
-       div(classes = "footer-container") {
-           div(classes = "footer-copyright") { +"© ${java.time.Year.now().value} BudgetAI" }
-           div(classes = "footer-links-container") {
-               links.forEach { link ->
-                   a(href = link.href, classes = "footer-link") { +link.text }
-               }
-           }
-       }
-   }
+val links = listOf(
+    FooterLink("Privacy", "/privacy"), FooterLink("Terms", "/terms")
+)
+
+fun FlowContent.Footer() {
+    footer(classes = "footer") {
+        div(classes = "footer-container") {
+            div(classes = "footer-copyright") { +"© ${java.time.Year.now().value} BudgetAI" }
+            div(classes = "footer-links-container") {
+                links.forEach { link ->
+                    a(href = link.href, classes = "footer-link") { +link.text }
+                }
+            }
+        }
+    }
 }
