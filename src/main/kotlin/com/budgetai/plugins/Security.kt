04b443db323c77e162516615f91ef2f6bb2b2895
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 13:34:27 +0000

    get code jwt from env var

--- src/main/kotlin/com/budgetai/plugins/Security.kt
+++ src/main/kotlin/com/budgetai/plugins/Security.kt
@@ -5,18 +5,21 @@ import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.config.*
 
-fun Application.configureSecurity() {
-    // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
+fun Application.configureSecurity(config: ApplicationConfig) {
+    val jwtAudience = config.property("jwt.audience").getString()
+    val jwtIssuer = config.property("jwt.issuer").getString()
+    val jwtRealm = config.property("jwt.realm").getString()
+    val jwtSecret = config.property("jwt.secret").getString()
+
+    val tokenExpiration = 60 * 60 * 24
+
     authentication {
         jwt {
             realm = jwtRealm
             verifier(
-                JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtDomain).build()
+                JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtIssuer).build()
             )
             validate { credential ->
                 if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
