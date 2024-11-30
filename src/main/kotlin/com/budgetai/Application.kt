751a560fcedf41290f1dc76bec91715adf21086b
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 17:58:44 +0000

    app config

--- src/main/kotlin/com/budgetai/Application.kt
+++ src/main/kotlin/com/budgetai/Application.kt
@@ -2,19 +2,22 @@ package com.budgetai
 
 import com.budgetai.lib.DataSeeder
 import com.budgetai.plugins.*
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     System.setProperty("io.ktor.development", "true")
-    System.setProperty("config.resource", "application-development.conf")
+    System.setProperty("config.resource", "development.conf")
     embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
-    val config = environment.config
+    val config = HoconApplicationConfig(ConfigFactory.load())
+
     val database = DatabaseConfig.initialize()
 
     val isDevelopment = checkDevelopmentMode(this)
@@ -31,6 +34,7 @@ fun Application.module() {
     } else {
         log.info("Running in production mode - skipping database seeding")
     }
+
     configureSecurity(config)
     configureHTTP()
     configureSerialization()
