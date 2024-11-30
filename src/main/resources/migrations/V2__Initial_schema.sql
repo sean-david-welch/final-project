fad8ad1e0e38fc73e5b05e4aef54b64543793852
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 16:03:41 +0000

    name

--- src/main/resources/migrations/V2__Initial_schema.sql
+++ src/main/resources/migrations/V2__Initial_schema.sql
@@ -2,10 +2,10 @@
 CREATE TABLE users
 (
     id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    email         VARCHAR(255) NOT NULL UNIQUE,
-    role          VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
     name          VARCHAR(100) NOT NULL,
+    role          VARCHAR(255) NOT NULL,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
     created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
