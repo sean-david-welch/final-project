25e9c8063d70f81f08c54a715c4ac591988b8007
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 12:29:29 +0000

    create home routes

---
+++ src/main/kotlin/com/budgetai/routes/templates/HomeRoutes.kt
@@ -0,0 +1,14 @@
+package com.budgetai.routes.templates
+
+import com.budgetai.templates.pages.createDashboardPage
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.homeRoutes() {
+        get("/") {
+            call.respondText(
+                text = createDashboardPage(), contentType = ContentType.Text.Html
+            )
+        }
+}
