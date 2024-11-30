e9a03cb93f2a4d657321b895b918dfea56a29601
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 12:50:37 +0000

    dashboard route fix

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -7,7 +7,7 @@ import io.ktor.server.routing.*
 
 fun Route.dashboardRoutes() {
     route("/dashboard") {
-        get("/") {
+        get("") {
             call.respondText(
                 text = createDashboardPage(), contentType = ContentType.Text.Html
             )
