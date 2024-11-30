ad7e58bc80f4fc1ebb182b7ecb7e43ece3284674
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 09:37:25 +0000

    home template

---
+++ src/main/kotlin/com/budgetai/templates/pages/Home.html.kt
@@ -0,0 +1,8 @@
+package com.budgetai.templates.pages
+
+import com.budgetai.templates.layout.BaseTemplate
+import com.budgetai.templates.layout.Navbar
+
+fun HomeTemplate() = BaseTemplate {
+    Navbar()
+}
