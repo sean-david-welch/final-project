2bcb1d8944a9ce978641802af9bb2450dd4a0512
Sean Welch <seanwelch51199@icloud.com>
2024-11-30 17:34:59 +0000

    auth header

--- src/main/kotlin/com/budgetai/plugins/Security.kt
+++ src/main/kotlin/com/budgetai/plugins/Security.kt
@@ -3,6 +3,7 @@ package com.budgetai.plugins
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
+import io.ktor.http.auth.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -17,7 +18,7 @@ fun Application.configureSecurity(config: ApplicationConfig) {
     val jwtSecret = config.property("jwt.secret").getString()
 
     authentication {
-        jwt {
+        jwt("auth-jwt") {
             realm = jwtRealm
             verifier(
                 JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtIssuer).build()
@@ -27,6 +28,11 @@ fun Application.configureSecurity(config: ApplicationConfig) {
             }
             challenge { _, _ ->
                 call.response.status(HttpStatusCode.Unauthorized)
+            }
+            authHeader { call ->
+                call.request.cookies["jwt_token"]?.let { token ->
+                    HttpAuthHeader.Single("Bearer", token)
+                }
             }
         }
     }
