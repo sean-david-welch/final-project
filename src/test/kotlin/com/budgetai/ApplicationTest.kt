a895b17415225e5bc4ef4e6b85e0405e12c1b4da
Sean Welch <seanwelch51199@icloud.com>
2024-12-01 15:40:24 +0000

    application test fix

--- src/test/kotlin/com/budgetai/ApplicationTest.kt
+++ src/test/kotlin/com/budgetai/ApplicationTest.kt
@@ -3,14 +3,17 @@ package com.budgetai
 import com.budgetai.plugins.configureHTTP
 import com.budgetai.plugins.configureSecurity
 import com.budgetai.plugins.configureSerialization
+import com.typesafe.config.ConfigFactory
+import io.ktor.server.config.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        val config = HoconApplicationConfig(ConfigFactory.load())
         application {
-            configureSecurity()
+            configureSecurity(config)
             configureHTTP()
             configureSerialization()
         }
