f642cb93fa8ce0ec593770ccb1a8563817981f91
Sean Welch <seanwelch51199@icloud.com>
2024-12-01 17:46:25 +0000

    removal

--- src/main/kotlin/com/budgetai/plugins/Security.kt
+++ src/main/kotlin/com/budgetai/plugins/Security.kt
@@ -12,41 +12,31 @@ import io.ktor.server.config.*
 const val TOKEN_EXPIRATION = 60 * 60 * 24
 
 fun Application.configureSecurity(config: ApplicationConfig, isTest: Boolean = false) {
-    if (isTest) {
-        authentication {
-            basic("auth-basic") {
-                validate { _ ->
-                    UserIdPrincipal("test-user")
-                }
-            }
-        }
-        return
-    } else {
-        val jwtConfig = config.config("jwt")
-        val jwtAudience = jwtConfig.property("audience").getString()
-        val jwtIssuer = jwtConfig.property("issuer").getString()
-        val jwtRealm = jwtConfig.property("realm").getString()
-        val jwtSecret = jwtConfig.property("secret").getString()
+    val jwtConfig = config.config("jwt")
+    val jwtAudience = jwtConfig.property("audience").getString()
+    val jwtIssuer = jwtConfig.property("issuer").getString()
+    val jwtRealm = jwtConfig.property("realm").getString()
+    val jwtSecret = jwtConfig.property("secret").getString()
 
-        authentication {
-            jwt("auth-jwt") {
-                realm = jwtRealm
-                verifier(
-                    JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtIssuer).build()
-                )
-                validate { credential ->
-                    if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
-                }
-                challenge { _, _ ->
-                    call.response.status(HttpStatusCode.Unauthorized)
-                }
-                authHeader { call ->
-                    call.request.cookies["jwt_token"]?.let { token ->
-                        HttpAuthHeader.Single("Bearer", token)
-                    }
+    authentication {
+        jwt("auth-jwt") {
+            realm = jwtRealm
+            verifier(
+                JWT.require(Algorithm.HMAC256(jwtSecret)).withAudience(jwtAudience).withIssuer(jwtIssuer).build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+            challenge { _, _ ->
+                call.response.status(HttpStatusCode.Unauthorized)
+            }
+            authHeader { call ->
+                call.request.cookies["jwt_token"]?.let { token ->
+                    HttpAuthHeader.Single("Bearer", token)
                 }
             }
         }
+
     }
 }
 
