ffae4e7ab7265a3506ec55a55b36c96cd865c1cc
Sean Welch <seanwelch51199@icloud.com>
2024-12-02 18:43:01 +0000

    add auth routes

--- src/main/kotlin/com/budgetai/routes/templates/MainRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/MainRoutes.kt
@@ -1,5 +1,6 @@
 package com.budgetai.routes.templates
 
+import com.budgetai.templates.pages.createAuthPage
 import com.budgetai.templates.pages.createHomePage
 import io.ktor.http.*
 import io.ktor.server.response.*
@@ -7,8 +8,9 @@ import io.ktor.server.routing.*
 
 fun Route.mainRoutes() {
     get("/") {
-        call.respondText(
-            text = createHomePage(), contentType = ContentType.Text.Html
-        )
+        call.respondText(text = createHomePage(), contentType = ContentType.Text.Html)
+    }
+    get("/auth") {
+        call.respondText(text = createAuthPage(), contentType = ContentType.Text.Html)
     }
 }
