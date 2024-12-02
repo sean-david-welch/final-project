befa37e94b856616346f6fd69738a0c73fbaca6d
Sean Welch <seanwelch51199@icloud.com>
2024-12-02 17:59:28 +0000

    fix other mistake

--- src/main/kotlin/com/budgetai/routes/templates/ReportsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/ReportsRoutes.kt
@@ -1,6 +1,6 @@
 package com.budgetai.routes.templates
 
-import com.budgetai.templates.pages.createDashboardPage
+import com.budgetai.templates.pages.createReportsPage
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,7 +9,7 @@ fun Route.reportRoutes() {
     route("/reports") {
         get("") {
             call.respondText(
-                text = createDashboardPage(), contentType = ContentType.Text.Html
+                text = createReportsPage(), contentType = ContentType.Text.Html
             )
         }
     }
