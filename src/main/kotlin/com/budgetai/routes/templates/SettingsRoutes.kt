1e8c20d02925ab06ae3d9e9555c852680bd87fc0
Sean Welch <seanwelch51199@icloud.com>
2024-12-02 17:58:15 +0000

    add settings

--- src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
@@ -1,1 +1,16 @@
 package com.budgetai.routes.templates
+
+import com.budgetai.templates.pages.createDashboardPage
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.settingsRoutes() {
+    route("/reports") {
+        get("") {
+            call.respondText(
+                text = createDashboardPage(), contentType = ContentType.Text.Html
+            )
+        }
+    }
+}
