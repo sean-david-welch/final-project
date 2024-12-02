e7b2aa219d3062fd14c9eccbc60f9599615ee4df
Sean Welch <seanwelch51199@icloud.com>
2024-12-02 17:40:30 +0000

    more

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "src/main/resources/static/**/*.css",
-    "src/main/kotlin/com/budgetai/templates/**/*.kt"
+    "src/main/kotlin/com/budgetai/templates/**/**/*.kt"
   ],
   theme: {
     extend: {},
