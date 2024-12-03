3fbed389f082781c86a7038a47de59a93907598a
Sean Welch <seanwelch51199@icloud.com>
2024-12-03 21:34:49 +0000

    lint

--- src/main/kotlin/com/budgetai/routes/api/AiInsightRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/api/AiInsightRoutes.kt
@@ -4,7 +4,6 @@ import com.budgetai.models.*
 import com.budgetai.routes.middleware.requireAuth
 import com.budgetai.services.AiInsightService
 import io.ktor.http.*
-import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
