dc4ade6b3f3c67c70cf7ef7daf25c5abc6d04d72
Sean Welch <seanwelch51199@icloud.com>
2024-12-03 19:14:18 +0000

    downgrade htm

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -9,7 +9,7 @@ fun BaseTemplate(bodyFn: BODY.() -> Unit) = "<!DOCTYPE html>" + createHTML().htm
         meta { charset = "UTF-8" }
         meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
         script { src = "https://unpkg.com/alpinejs@3.14.3/dist/cdn.min.js"; defer = true }
-        script { src = "https://unpkg.com/htmx.org@2.0.3/dist/htmx.min.js"; defer = true }
+        script { src = "https://unpkg.com/htmx.org@1.9.3/dist/htmx.min.js"; defer = true }
         link { href = "/static/images/favicon.ico"; rel = "icon"; type = "image/x-icon" }
         link { href = "/static/styles/output.css"; rel = "stylesheet" }
         link { href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"; rel = "stylesheet" }
