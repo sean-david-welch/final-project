72a0b2f59b87055c7b842b4cd57f6581486bf658
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 21:34:49 +0000

    authenticate settings

--- src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
@@ -3,15 +3,16 @@ package com.budgetai.routes.templates
 import com.budgetai.templates.pages.createSettingsPage
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.settingsRoutes() {
-    route("/settings") {
-        get("") {
-            call.respondText(
-                text = createSettingsPage(call.templateContext), contentType = ContentType.Text.Html
-            )
+    authenticate {
+        route("/settings") {
+            get("") {
+                call.respondText(text = createSettingsPage(call.templateContext), contentType = ContentType.Text.Html)
+            }
         }
     }
 }
