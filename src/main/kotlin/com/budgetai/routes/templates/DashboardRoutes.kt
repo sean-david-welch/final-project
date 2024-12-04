3acb0c317cd17807b8e0453dca753ce3ea377c76
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 21:35:29 +0000

    dashboard

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -1,19 +1,16 @@
 package com.budgetai.routes.templates
 
 import com.budgetai.templates.pages.createDashboardPage
-import com.budgetai.utils.createTemplateContext
+import com.budgetai.utils.templateContext
 import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.dashboardRoutes() {
-    route("/dashboard") {
-        authenticate {
-            get {
-                val context = call.createTemplateContext()
-                call.respondText(text = createDashboardPage(context), contentType = ContentType.Text.Html)
-            }
+    authenticate {
+        route("/dashboard") {
+            get {call.respondText(text = createDashboardPage(call.templateContext), contentType = ContentType.Text.Html)}
         }
     }
 }
