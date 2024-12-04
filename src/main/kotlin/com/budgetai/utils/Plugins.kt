c788111ff146f6e9db2af12f30839447a3b3aea1
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 18:56:43 +0000

    lint

--- src/main/kotlin/com/budgetai/utils/Plugins.kt
+++ src/main/kotlin/com/budgetai/utils/Plugins.kt
@@ -1,9 +1,6 @@
 package com.budgetai.utils
 
-import com.budgetai.utils.BaseTemplateContext
-import com.budgetai.utils.createTemplateContext
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.util.*
 
 val TemplateContextKey = AttributeKey<BaseTemplateContext>("TemplateContext")
