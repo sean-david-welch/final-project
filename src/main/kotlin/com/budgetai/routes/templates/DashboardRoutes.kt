e7d5cb8a4f22149a4a9df1777e346b5601d9d0c0
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 17:40:17 +0000

    remove custom middleware routes dashboard

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -1,15 +1,15 @@
 package com.budgetai.routes.templates
 
-import com.budgetai.routes.middleware.requireAuth
 import com.budgetai.templates.pages.createDashboardPage
 import com.budgetai.utils.createTemplateContext
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.dashboardRoutes() {
     route("/dashboard") {
-        requireAuth {
+        authenticate {
             get("") {
                 val context = call.createTemplateContext()
                 call.respondText(text = createDashboardPage(context), contentType = ContentType.Text.Html)
