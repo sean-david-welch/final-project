d2fdd0999b90089c2151ffcb8c9f17665f6ea4ee
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 21:34:19 +0000

    authenticate

--- src/main/kotlin/com/budgetai/routes/templates/ReportsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/ReportsRoutes.kt
@@ -3,15 +3,18 @@ package com.budgetai.routes.templates
 import com.budgetai.templates.pages.createReportsPage
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.reportRoutes() {
-    route("/reports") {
-        get("") {
-            call.respondText(
-                text = createReportsPage(call.templateContext), contentType = ContentType.Text.Html
-            )
+    authenticate {
+        route("/reports") {
+            get("") {
+                call.respondText(
+                    text = createReportsPage(call.templateContext), contentType = ContentType.Text.Html
+                )
+            }
         }
     }
 }
