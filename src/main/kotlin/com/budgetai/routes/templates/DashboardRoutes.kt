940151be95ed9aeb49ae87e7f7710ef69f788308
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 17:22:31 +0000

    require auth

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -1,5 +1,6 @@
 package com.budgetai.routes.templates
 
+import com.budgetai.routes.middleware.requireAuth
 import com.budgetai.templates.pages.createDashboardPage
 import io.ktor.http.*
 import io.ktor.server.response.*
@@ -7,10 +8,12 @@ import io.ktor.server.routing.*
 
 fun Route.dashboardRoutes() {
     route("/dashboard") {
-        get("") {
-            call.respondText(
-                text = createDashboardPage(), contentType = ContentType.Text.Html
-            )
+        requireAuth {
+            get("") {
+                call.respondText(
+                    text = createDashboardPage(), contentType = ContentType.Text.Html
+                )
+            }
         }
     }
 }
