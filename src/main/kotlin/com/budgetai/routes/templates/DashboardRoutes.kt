cdfc01995148424de28af90937d5688aff457325
Sean Welch <seanwelch51199@icloud.com>
2024-12-04 17:26:10 +0000

    pass context to routes

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -2,6 +2,7 @@ package com.budgetai.routes.templates
 
 import com.budgetai.routes.middleware.requireAuth
 import com.budgetai.templates.pages.createDashboardPage
+import com.budgetai.utils.createTemplateContext
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -10,8 +11,10 @@ fun Route.dashboardRoutes() {
     route("/dashboard") {
         requireAuth {
             get("") {
+                        val context = call.createTemplateContext()
+
                 call.respondText(
-                    text = createDashboardPage(), contentType = ContentType.Text.Html
+                    text = createDashboardPage(context), contentType = ContentType.Text.Html
                 )
             }
         }
