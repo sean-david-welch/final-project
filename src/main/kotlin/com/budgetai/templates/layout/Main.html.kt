78c40146fd207c205460e54b46a538d0662f6e53
Sean Welch <seanwelch51199@icloud.com>
2024-12-05 19:35:55 +0000

    remove spreadsheets

--- src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
+++ src/main/kotlin/com/budgetai/templates/layout/Main.html.kt
@@ -11,10 +11,8 @@ fun BaseTemplate(context: BaseTemplateContext, bodyFn: BODY.() -> Unit) = "<!DOC
         meta { name = "viewport"; content = "width=device-width, initial-scale=1.0" }
         script { src = "https://unpkg.com/alpinejs@3.14.3/dist/cdn.min.js"; defer = true }
         script { src = "https://unpkg.com/htmx.org@1.9.3/dist/htmx.min.js"; defer = true }
-        script { src = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.js"; defer = true }
         link { href = "/static/images/favicon.ico"; rel = "icon"; type = "image/x-icon" }
         link { href = "/static/styles/output.css"; rel = "stylesheet" }
-        link { rel = "stylesheet"; href = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.css" }
         link { href = "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"; rel = "stylesheet" }
 
     }
