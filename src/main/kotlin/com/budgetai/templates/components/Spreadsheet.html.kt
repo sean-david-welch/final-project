c166dc888d297901a886fa91111de67ba1da06a6
Sean Welch <seanwelch51199@icloud.com>
2024-12-05 19:36:07 +0000

    spreadsheets move styles

--- src/main/kotlin/com/budgetai/templates/components/Spreadsheet.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Spreadsheet.html.kt
@@ -3,6 +3,8 @@ package com.budgetai.templates.components
 import kotlinx.html.*
 
 fun FlowContent.SpreadsheetComponent(id: String = "spreadsheet") {
+    link { rel = "stylesheet"; href = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.css" }
+    script { src = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.js"; defer = true }
     script(src = "/static/scripts/spreadsheet.js") {}
 
     div(classes = "spreadsheet-wrapper") {
