bc0ee240bbd2d375ceadb0743e3909103e06b469
Sean Welch <seanwelch51199@icloud.com>
2024-12-05 21:16:40 +0000

    get user in routess

--- src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
@@ -1,17 +1,21 @@
 package com.budgetai.routes.templates
 
+import com.budgetai.services.UserService
 import com.budgetai.templates.pages.createSettingsPage
+import com.budgetai.utils.UserPrincipal
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.settingsRoutes() {
+fun Route.settingsRoutes(userService: UserService) {
     authenticate {
         route("/settings") {
-            get("") {
-                call.respondText(text = createSettingsPage(call.templateContext), contentType = ContentType.Text.Html)
+            get {
+                val principal = call.principal<UserPrincipal>() ?: throw IllegalArgumentException("User not authenticated")
+                val user = userService.getUser(principal.id.toInt()) ?: throw IllegalArgumentException("User not found")
+                call.respondText(text = createSettingsPage(call.templateContext, user), contentType = ContentType.Text.Html)
             }
         }
     }
