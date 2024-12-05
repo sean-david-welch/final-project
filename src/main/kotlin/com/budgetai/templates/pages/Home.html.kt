9c5802b5e6e430ccf5c9920d90a4d9e27fabfdd5
Sean Welch <seanwelch51199@icloud.com>
2024-12-05 19:39:23 +0000

    remove

--- src/main/kotlin/com/budgetai/templates/pages/Home.html.kt
+++ src/main/kotlin/com/budgetai/templates/pages/Home.html.kt
@@ -25,8 +25,7 @@ fun createHomePage(context: BaseTemplateContext) = HomeTemplate("Welcome to Budg
                 +"Smart budgeting powered by artificial intelligence to help you achieve your financial goals."
             }
             div(classes = "hero-actions") {
-                a(href = "/dashboard", classes = "primary-button") { +"Get Started" }
-                a(href = "/learn-more", classes = "secondary-button") { +"Learn More" }
+                a(href = "/login", classes = "primary-button") { +"Get Started" }
             }
         }
     }
