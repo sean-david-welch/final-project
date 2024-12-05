6afaa07cd73ff0f6b7ea138eeec166c4e08647ef
Sean Welch <seanwelch51199@icloud.com>
2024-12-05 21:21:16 +0000

    fix user

--- src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/SettingsRoutes.kt
@@ -2,7 +2,6 @@ package com.budgetai.routes.templates
 
 import com.budgetai.services.UserService
 import com.budgetai.templates.pages.createSettingsPage
-import com.budgetai.utils.UserPrincipal
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
 import io.ktor.server.auth.*
@@ -13,8 +12,7 @@ fun Route.settingsRoutes(userService: UserService) {
     authenticate {
         route("/settings") {
             get {
-                val principal = call.principal<UserPrincipal>() ?: throw IllegalArgumentException("User not authenticated")
-                val user = userService.getUser(principal.id.toInt()) ?: throw IllegalArgumentException("User not found")
+                val user = call.templateContext.auth.user?.id?.let { userService.getUser(it.toInt()) } ?: throw IllegalArgumentException("User not found")
                 call.respondText(text = createSettingsPage(call.templateContext, user), contentType = ContentType.Text.Html)
             }
         }
