38be508fcc8dcedef32db953d2f1b19c790f83dd
Sean Welch <seanwelch51199@icloud.com>
2024-12-06 17:29:29 +0000

    add users to templ

--- src/main/kotlin/com/budgetai/routes/templates/MainRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/MainRoutes.kt
@@ -1,12 +1,13 @@
 package com.budgetai.routes.templates
 
+import com.budgetai.services.UserService
 import com.budgetai.templates.pages.*
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.mainRoutes() {
+fun Route.mainRoutes(userService: UserService) {
     get("/") {
         call.respondText(text = createHomePage(call.templateContext), contentType = ContentType.Text.Html)
     }
@@ -21,7 +22,8 @@ fun Route.mainRoutes() {
         if (!call.templateContext.auth.isAdmin) {
             call.respondText(text = create403Page(call.templateContext), contentType = ContentType.Text.Html)
         } else {
-            call.respondText(text = createAdminPage(call.templateContext), contentType = ContentType.Text.Html)
+            val users = userService.getUsers()
+            call.respondText(text = createAdminPage(call.templateContext, users), contentType = ContentType.Text.Html)
         }
     }
     get("/test-error") {
