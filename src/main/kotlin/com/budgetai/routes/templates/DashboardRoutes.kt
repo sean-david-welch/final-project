97e3c6e9fb3f2b33a96c75a3de31e8ca9fa98272
Sean Welch <seanwelch51199@icloud.com>
2024-12-06 18:36:48 +0000

    move

--- src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
+++ src/main/kotlin/com/budgetai/routes/templates/DashboardRoutes.kt
@@ -1,5 +1,9 @@
 package com.budgetai.routes.templates
 
+import com.budgetai.services.BudgetItemService
+import com.budgetai.services.BudgetService
+import com.budgetai.services.CategoryService
+import com.budgetai.services.UserService
 import com.budgetai.templates.pages.createDashboardPage
 import com.budgetai.utils.templateContext
 import io.ktor.http.*
@@ -7,10 +11,16 @@ import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route.dashboardRoutes() {
+fun Route.dashboardRoutes(budgetItemService: BudgetItemService, budgetService: BudgetService, categoryService: CategoryService) {
     authenticate {
         route("/dashboard") {
-            get {call.respondText(text = createDashboardPage(call.templateContext), contentType = ContentType.Text.Html)}
+
+            get {
+                val users = budgetItemService.getUsers()
+                val budgets = budgetService.getBudgets()
+                val categories = categoryService.getCategories()
+                call.respondText(text = createDashboardPage(call.templateContext), contentType = ContentType.Text.Html)
+            }
         }
     }
 }
