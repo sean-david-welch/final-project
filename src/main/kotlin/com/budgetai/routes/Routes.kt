9940fe3071c1ea756c534921ba125fd79c72d832
Sean Welch <seanwelch51199@icloud.com>
2024-12-06 18:39:10 +0000

    pass user servicee

--- src/main/kotlin/com/budgetai/routes/Routes.kt
+++ src/main/kotlin/com/budgetai/routes/Routes.kt
@@ -41,7 +41,7 @@ fun Application.configureRoutes(config: ApplicationConfig, database: Database? =
         // Template routes
         authRoutes(userService)
         mainRoutes(userService, budgetService, categoryService)
-        dashboardRoutes(budgetItemService, budgetService, categoryService)
+        dashboardRoutes(userService, budgetItemService, budgetService, categoryService)
         reportRoutes()
         settingsRoutes(userService)
 
