ac293d8e48d15ebbcd43e6d18275dc01db3b9729
Sean Welch <seanwelch51199@icloud.com>
2024-12-07 08:51:23 +0000

    rename spread

--- src/main/kotlin/com/budgetai/templates/components/Spreadsheet.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Spreadsheet.html.kt
@@ -1,26 +1,51 @@
-//package com.budgetai.templates.components
-//
-//import kotlinx.html.*
-//
-//fun FlowContent.SpreadsheetComponent(id: String = "spreadsheet") {
-//    link { rel = "stylesheet"; href = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.css" }
-//    script { src = "https://cdn.jsdelivr.net/npm/handsontable@latest/dist/handsontable.full.min.js"; defer = true }
-//    script(src = "/static/scripts/spreadsheet.js") {}
-//
-//    div(classes = "spreadsheet-wrapper") {
-//        div(classes = "spreadsheet-controls") {
-//            button(classes = "control-button") {
-//                attributes["onclick"] = "spreadsheetManager.addRow()"
-//                +"Add Row"
-//            }
-//            button(classes = "control-button") {
-//                attributes["onclick"] = "spreadsheetManager.addColumn()"
-//                +"Add Column"
-//            }
-//        }
-//        div {
-//            this.id = id
-//            classes = setOf("spreadsheet-container")
-//        }
-//    }
-//}
+package com.budgetai.templates.components
+
+import kotlinx.html.*
+
+fun FlowContent.SpreadsheetComponent() {
+    div(classes = "spreadsheet-wrapper") {
+        div {
+            h2(classes = "spreadsheet-title") { +"Data Entry" }
+        }
+
+        table(classes = "spreadsheet-container") {
+            thead {
+                tr {
+                    th(classes = "spreadsheet-header") {
+                        contentEditable = true
+                        +"Header 1"
+                    }
+                    th(classes = "spreadsheet-header") {
+                        contentEditable = true
+                        +"Header 2"
+                    }
+                    th(classes = "spreadsheet-header") {
+                        contentEditable = true
+                        +"Header 3"
+                    }
+                }
+            }
+            tbody {
+                tr {
+                    td(classes = "spreadsheet-cell") {
+                        contentEditable = true
+                        +""
+                    }
+                    td(classes = "spreadsheet-cell") {
+                        contentEditable = true
+                        +""
+                    }
+                    td(classes = "spreadsheet-cell") {
+                        contentEditable = true
+                        +""
+                    }
+                }
+            }
+        }
+
+        button(classes = "spreadsheet-add-row") {
+            onClick = "new EditableTable('spreadsheet-container')"
+            +"Add Row"
+        }
+    }
+}
