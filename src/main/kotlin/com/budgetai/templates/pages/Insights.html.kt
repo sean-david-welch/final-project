debe5ac44697ece24112e21cada85e5ac29cdf60
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 20:10:18 +0000

    remove

--- src/main/kotlin/com/budgetai/templates/pages/Insights.html.kt
+++ src/main/kotlin/com/budgetai/templates/pages/Insights.html.kt
@@ -15,20 +15,10 @@ fun AIInsightsTemplate(context: BaseTemplateContext, budgets: List<BudgetDTO>) =
                 +"Get personalized insights and recommendations for your budgets"
             }
         }
-        button(classes = "action-button") {
-            attributes["onclick"] = "document.getElementById('modal-dialog').showModal()"
-            +"New Analysis"
-        }
     }
 
     // Main Content
     div(classes = "content-grid") {
-        h2(classes = "heading-large") {
-            +"Generate New Analysis"
-        }
-        div(classes = "text-base mt-4") {
-            +"Select a budget and analysis type to get personalized insights"
-        }
         div {
             attributes["id"] = "response-message"
         }
