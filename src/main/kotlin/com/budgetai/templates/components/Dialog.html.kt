09fb50f74480f925c52d93b507340962f65bbc5b
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 09:28:25 +0000

    include js

--- src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
@@ -5,21 +5,10 @@ import kotlinx.html.*
 
 fun FlowContent.DialogComponent(context: BaseTemplateContext, content: FlowContent.() -> Unit) {
     if (context.auth.isAuthenticated) {
+        script(src = "/static/scripts/dialog.js") {}
+
         dialog {
-            attributes["x-data"] = """
-                {
-                    visible: false,
-                    show() { 
-                        this.${'$'}el.showModal();
-                        this.${'$'}el.classList.add('dialog-visible');
-                    },
-                    close() { 
-                        this.${'$'}el.close();
-                        this.${'$'}el.classList.remove('dialog-visible');
-                        this.${'$'}dispatch('dialog-closed');
-                    }
-                }
-            """.trimIndent()
+            attributes["x-data"] = "dialogComponent()"
             attributes["x-show"] = "visible"
             attributes["@show-dialog.window"] = "show()"
             attributes["@hide-dialog.window"] = "close()"
