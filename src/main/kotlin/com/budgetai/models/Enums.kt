9e6af2abde681f6416a111c1c2a526527e45220d
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 18:09:42 +0000

    lint

--- src/main/kotlin/com/budgetai/models/Enums.kt
+++ src/main/kotlin/com/budgetai/models/Enums.kt
@@ -13,26 +13,40 @@ enum class Sentiment {
 
 @Serializable
 enum class PromptType {
-    @SerialName("COST_REDUCTION") COST_REDUCTION,
-    @SerialName("PRICE_ALTERNATIVES") PRICE_ALTERNATIVES,
-    @SerialName("SPENDING_PATTERNS") SPENDING_PATTERNS,
-    @SerialName("CATEGORY_ANALYSIS") CATEGORY_ANALYSIS,
-    @SerialName("CUSTOM_ANALYSIS") CUSTOM_ANALYSIS
+    @SerialName("COST_REDUCTION")
+    COST_REDUCTION,
+    @SerialName("PRICE_ALTERNATIVES")
+    PRICE_ALTERNATIVES,
+    @SerialName("SPENDING_PATTERNS")
+    SPENDING_PATTERNS,
+    @SerialName("CATEGORY_ANALYSIS")
+    CATEGORY_ANALYSIS,
+    @SerialName("CUSTOM_ANALYSIS")
+    CUSTOM_ANALYSIS
 }
 
 @Serializable
 enum class CategoryType {
-    @SerialName("FIXED") FIXED,
-    @SerialName("VARIABLE") VARIABLE,
-    @SerialName("RECURRING") RECURRING,
-    @SerialName("EMERGENCY") EMERGENCY,
-    @SerialName("DISCRETIONARY") DISCRETIONARY,
-    @SerialName("NECESSARY") NECESSARY
+    @SerialName("FIXED")
+    FIXED,
+    @SerialName("VARIABLE")
+    VARIABLE,
+    @SerialName("RECURRING")
+    RECURRING,
+    @SerialName("EMERGENCY")
+    EMERGENCY,
+    @SerialName("DISCRETIONARY")
+    DISCRETIONARY,
+    @SerialName("NECESSARY")
+    NECESSARY
 }
 
 @Serializable
 enum class UserRole {
-    @SerialName("ADMIN") ADMIN,
-    @SerialName("USER") USER,
-    @SerialName("GUEST") GUEST,
+    @SerialName("ADMIN")
+    ADMIN,
+    @SerialName("USER")
+    USER,
+    @SerialName("GUEST")
+    GUEST,
 }
