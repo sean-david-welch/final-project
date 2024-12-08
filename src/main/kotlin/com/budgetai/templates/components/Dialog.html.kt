64cfc47c2682a4be967bae7d926975576b238962
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 09:24:58 +0000

    content

--- src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
@@ -7,36 +7,38 @@ fun FlowContent.DialogComponent(
     context: BaseTemplateContext,
     content: FlowContent.() -> Unit
 ) {
-    dialog {
-        attributes["x-data"] = """
-            {
-                visible: false,
-                show() { 
-                    this.${'$'}el.showModal();
-                    this.${'$'}el.classList.add('dialog-visible');
-                },
-                close() { 
-                    this.${'$'}el.close();
-                    this.${'$'}el.classList.remove('dialog-visible');
-                    this.${'$'}dispatch('dialog-closed');
+    if (context.auth.isAuthenticated) {
+        dialog {
+            attributes["x-data"] = """
+                {
+                    visible: false,
+                    show() { 
+                        this.${'$'}el.showModal();
+                        this.${'$'}el.classList.add('dialog-visible');
+                    },
+                    close() { 
+                        this.${'$'}el.close();
+                        this.${'$'}el.classList.remove('dialog-visible');
+                        this.${'$'}dispatch('dialog-closed');
+                    }
                 }
+            """.trimIndent()
+            attributes["x-show"] = "visible"
+            attributes["@show-dialog.window"] = "show()"
+            attributes["@hide-dialog.window"] = "close()"
+            attributes["class"] = "dialog"
+
+            // Content slot
+            div(classes = "dialog-content") {
+                content()
             }
-        """.trimIndent()
-        attributes["x-show"] = "visible"
-        attributes["@show-dialog.window"] = "show()"
-        attributes["@hide-dialog.window"] = "close()"
-        attributes["class"] = "dialog"
 
-        // Content slot
-        div(classes = "dialog-content") {
-            content()
-        }
-
-        // Close button
-        button(classes = "dialog-btn") {
-            attributes["@click"] = "close()"
-            attributes["formmethod"] = "dialog"
-            +"×"
+            // Close button
+            button(classes = "dialog-btn") {
+                attributes["@click"] = "close()"
+                attributes["formmethod"] = "dialog"
+                +"×"
+            }
         }
     }
 }
