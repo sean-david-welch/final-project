5973efb580bbe1c52e2824a624e4afaf7359d0d3
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 09:55:40 +0000

    init

--- src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
+++ src/main/kotlin/com/budgetai/templates/components/Dialog.html.kt
@@ -7,23 +7,29 @@ fun FlowContent.DialogComponent(context: BaseTemplateContext, content: FlowConte
     if (context.auth.isAuthenticated) {
         script(src = "/static/scripts/dialog.js") {}
 
-        dialog {
+        div {
+            // Wrap dialog in a div with x-data to ensure proper Alpine.js scope
             attributes["x-data"] = "dialogComponent()"
-            attributes["x-show"] = "visible"
-            attributes["x-on:show-dialog.window"] = "show()"
-            attributes["x-on:hide-dialog.window"] = "close()"
-            attributes["class"] = "dialog"
 
-            // Content slot
-            div(classes = "dialog-content") {
-                content()
-            }
+            dialog {
+                // Remove x-data from here since it's now on parent
+                attributes["x-show"] = "visible"
+                attributes["x-on:show-dialog.window"] = "show"  // Remove parentheses
+                attributes["x-on:hide-dialog.window"] = "close" // Remove parentheses
+                attributes["class"] = "dialog"
+                attributes[":class"] = "{'dialog-visible': visible}"
 
-            // Close button
-            button(classes = "dialog-btn") {
-                attributes["x-on:click"] = "close()"
-                attributes["formmethod"] = "dialog"
-                +"×"
+                // Content slot
+                div(classes = "dialog-content") {
+                    content()
+                }
+
+                // Close button
+                button(classes = "dialog-btn") {
+                    attributes["x-on:click"] = "close"  // Remove parentheses
+                    attributes["formmethod"] = "dialog"
+                    +"×"
+                }
             }
         }
     }
