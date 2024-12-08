f206475b8c367df6092fde0b4e7aedc31786fbc9
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 21:06:36 +0000

    revmoe

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,6 @@ version = "0.0.1"
 
 application {
     mainClass.set("com.budgetai.ApplicationKt")
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
