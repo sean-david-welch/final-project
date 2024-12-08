ab5bbc2ea0df0fc28bf3529c51294d95eac46ed3
Sean Welch <seanwelch51199@icloud.com>
2024-12-08 20:42:09 +0000

    config

---
+++ eb-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load .env file
+set -a
+source .env
+set +a
+
+# Create eb extensions directory if it doesn't exist
+mkdir -p .ebextensions
+
+# Use envsubst to replace variables
+envsubst < .ebextensions/env.config.template > .ebextensions/env.config
